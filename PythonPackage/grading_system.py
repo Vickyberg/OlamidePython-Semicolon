85ac1b59a37e2e52de2fde43a98e69366e892369
Vickyberg <akinnusivictor098@gmail.com>
2022-06-02 12:57:32 +0100

    Python program

---
+++ PythonPackage/grading_system.py
@@ -0,0 +1,13 @@
+grade = int(input("Enter student grade: "))
+if grade >= 70:
+    print("A")
+if 60 <= grade <= 69:
+    print("B")
+if 50 <= grade <= 59:
+    print("C")
+if 45 <= grade <= 49:
+    print("D")
+if 40 <= grade <= 44:
+    print("E")
+else:
+    print("F")
---
+++ PythonPackage/largest_number.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter the first value: "))
+number2 = int(input("Enter the second value: "))
+if number1 > number2:
+    print("First values is greater than second value")
+else:
+    print("Second values is greater than first value")
