38314a23e9301c7faf1eae8bb48da4f75a910381
Victor Olamide <akinnusivictor098@gmail.com>
2022-07-21 11:29:48 +0100

    Working with Yaml

--- PythonPackage/Yaml/yaml_practice.py
+++ PythonPackage/Yaml/yaml_practice.py
@@ -4,4 +4,10 @@ import yaml
 file_path = pathlib.Path("./config.yaml").resolve()
 text = {'name': 'Ola', 'age': 18, 'children': ["Ope", "Oye"]}
 with file_path.open(mode="w") as f:
-    yaml.dump(text, f)
+    yaml.dump(text, f, sort_keys=False)
+
+with file_path.open(mode='r', encoding='utf-8') as file:
+    text_two = yaml.load(file, Loader=yaml.Loader)
+    print(text)
+    print(text_two)
+# Both are used to save files
