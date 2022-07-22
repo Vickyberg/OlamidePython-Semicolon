7e185faf1e48a6a953a3d9915ef26415a913e969
Victor Olamide <akinnusivictor098@gmail.com>
2022-07-22 11:26:44 +0100

    Working Setter and Getter

--- PythonPackage/Python_Classes/first_class.py
+++ PythonPackage/Python_Classes/first_class.py
@@ -1,22 +1,38 @@
 class Person:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, age: 17) -> None:
         self._name = name
+        self._age = age
 
     @property
     def name(self):
-        print("You're calling me")
         return self._name
 
     @name.setter
-    def name(self, name):
-        print("Setter")
-        if "f" in name:
-            print("Fuck Off")
-            return
-        self._name = name
+    def name(self, new_name):
+        self._name = new_name
+
+    @name.deleter
+    def name(self):
+        print("Name will now be deleted")
+        del self._name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, new_age):
+        if new_age < 0:
+            raise ValueError("Age cannot be negative")
+        self._age = new_age
 
 
-person1 = Person("Abigail")
-person1.name = "Omotee f"
+person1 = Person("Abigail",33)
+person2 = Person("Dorcas",56)
 
 print(person1.name)
+person1.name = "Ola"
+
+del person1.name
+print(person1.name)
+print(dir(person1))
